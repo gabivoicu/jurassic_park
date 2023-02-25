8994d91a1866a50984f089695cfca33f865a490a
Gabriela Voicu <gabrielavoicu16@gmail.com>
2023-02-24 18:21:32 -0600

    Add cages controller
    show, create and update cages per reqs:
    herbivore dinosaurs cage with other herbivore
    carnivore dinosaurs cage with only their species

---
+++ app/controllers/api/v1/cages_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::CagesController < ApplicationController
+  def show
+    cages = Cage.all.as_json(methods: [:dinosaurs, :dinosaur_count])
+
+    render json: { cages: }, formats: :json
+  end
+
+  def create
+    Cage.create_new!(cage_params)
+    render json: { message: "Cage created successfully" }, formats: :json, status: :created
+  rescue StandardError => e
+    render json: { error: e.message }, status: :bad_request
+  end
+
+  def update
+    cage = Cage.find(params[:id])
+    cage.add_dinosaurs(cage_params[:dinosaurs])
+
+    render json: { message: "Cage updated successfully" }, formats: :json, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :bad_request
+  end
+
+  private
+
+  def cage_params
+    params.require(:cage).permit(dinosaurs: [])
+  end
+end
--- app/models/cage.rb
+++ app/models/cage.rb
@@ -2,4 +2,36 @@
 
 class Cage < ApplicationRecord
   has_many :dinosaurs, dependent: :nullify
+
+  HERBIVORE_ERROR_MESSAGE = "Herbivore dinosaurs can only be kept with other herbivores."
+  CARNIVORE_ERROR_MESSAGE = "Carnivore dinosaurs can only be kept with their own species."
+
+  def self.create_new!(params)
+    ActiveRecord::Base.transaction do
+      cage = create!
+      cage.add_dinosaurs(params[:dinosaurs])
+    end
+  end
+
+  def add_dinosaurs(dinosaur_ids)
+    dinosaurs = Dinosaur.find(dinosaur_ids)
+    dino = dinosaurs.first
+
+    if dino.herbivore?
+      unless dinosaurs.all?(&:herbivore?)
+        raise ArgumentError,
+              HERBIVORE_ERROR_MESSAGE
+      end
+    else
+      raise ArgumentError, CARNIVORE_ERROR_MESSAGE unless dinosaurs.all? do |d|
+                                                            d.species == dino.species
+                                                          end
+    end
+
+    dinosaurs << dinosaurs
+  end
+
+  def dinosaur_count
+    dinosaurs.count
+  end
 end
