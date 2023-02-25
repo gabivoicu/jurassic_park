aa17ee975eb4ea7fae3abcb8340ca053176cc436
Gabriela Voicu <gabrielavoicu16@gmail.com>
2023-02-24 18:28:30 -0600

    Update specs with species requirement

--- spec/models/cage_spec.rb
+++ spec/models/cage_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Cage do
   context "with 2 or more dinosaurs" do
     it "orders them chronologically" do
       cage = described_class.create!
-      dinosaur1 = cage.dinosaurs.create!(name: "Dino 1")
-      dinosaur2 = cage.dinosaurs.create!(name: "Dino 2")
+      dinosaur1 = cage.dinosaurs.create!(name: "Dino 1", species: "Megalosaurus")
+      dinosaur2 = cage.dinosaurs.create!(name: "Dino 2", species: "Megalosaurus")
       expect(cage.reload.dinosaurs).to eq([dinosaur1, dinosaur2])
     end
   end
--- spec/models/dinosaur_spec.rb
+++ spec/models/dinosaur_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe Dinosaur do
   context "with name field" do
     it "is not valid without a name" do
-      dinosaur = described_class.create
+      dinosaur = described_class.create(species: "Megalosaurus")
       expect(dinosaur).not_to be_valid
     end
 
     it "is valid with a name" do
-      dinosaur = described_class.create(name: "Dino")
+      dinosaur = described_class.create(name: "Dino", species: "Megalosaurus")
       expect(dinosaur).to be_valid
     end
   end
